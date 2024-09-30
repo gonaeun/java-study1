95909c52ed87c189c2a3712105bbddb1cb4ef5d9
gonaeun <gonaeunn@gmail.com>
2024-09-30 16:30:21 +0900

    변수

--- java-start/src/variable/Var2.java
+++ java-start/src/variable/Var2.java
@@ -3,13 +3,13 @@ package variable;
 public class Var2 {
     public static void main(String[] args) {
         int a; // 변수 선언
-        a = 20; // 변수 초기화  10->20
+        a = 20; // 변수 초기화(선언한 변수에 처음으로 값을 대입해서 저장하는 것)
         // 자바에서 '=' 은 '오른쪽의 값을 왼쪽에 저장한다'는 의미
 
         System.out.println(a);
         System.out.println(a);
         System.out.println(a);
         // 그릇에 a라는 값을 담고, 그 값을 읽기
-        // a의 값만 변경하면 출력값을 편리하게 변경할 수 있음
+        // a의 값만 변경하면(10->20) 출력값을 편리하게 변경할 수 있음
     }
 }
--- java-start/src/variable/Var6.java
+++ java-start/src/variable/Var6.java
@@ -3,7 +3,15 @@ package variable;
 public class Var6 {
     public static void main(String[] args) {
         int a;
+        // sout를 주석처리하면 out폴더에 Var 생성되지만, 컴파일 코드엔 존재하지 않음
+        // >> 변수를 선언했지만 사용하지 않으므로, java가 최적화 한 것
+
 //        System.out.println(a);
         // JAVA에서 지역변수의 값을 초기화 하지 않으면, 컴파일 에러 발생
+        // 컴파일 되지 않음 == out 폴더에도 Var6 생성되지 않음
     }
 }
+
+
+
+
